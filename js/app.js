738f4d15c0e6ea93847775ce0a775b18c2dcc509
Maxim Tresk <djtresk@gmail.com>
2024-06-01 17:25:58 +0500

    chore: clean up

--- js/app.js
+++ js/app.js
@@ -1,39 +1,32 @@
-import '../scss/style.scss'
 import * as flsFunctions from './files/functions.js'
 import * as flsForms from './files/forms/forms.js'
 import * as flsScroll from './files/scroll/scroll.js'
-
-// flsFunctions.addTouchClass();
-// flsFunctions.addLoadedClass();
-// flsFunctions.menuInit();
-// flsFunctions.fullVHfix();
-// flsFunctions.scrollDirection()
-
-// flsFunctions.spoilers();
-// flsFunctions.tabs();
-// flsFunctions.showMore();
 // import './libs/popup.js'
-// flsForms.formQuantity();
-// flsForms.formRating();
 // import './libs/select.js'
 // import './files/forms/datepicker.js'
-// import "./files/forms/inputmask.js";
-// import "./files/forms/range.js";
-// flsForms.formFieldsInit({
-// 	viewPass: false,
-// 	autoHeight: false
-// });
-// import "./files/tippy.js";
-// import "./files/sliders.js";
-// import './files/scroll/smooth-scrollbar.js';
-// import './files/scroll/lazyload.js';
+// import './files/forms/inputmask.js'
+// import './files/forms/range.js'
+// import './files/sliders.js'
+// import './files/scroll/smooth-scrollbar.js'
 // import './libs/watcher.js'
-// flsScroll.pageNavigation();
-// flsScroll.headerScroll();
-// flsScroll.stickyBlock();
-// flsScroll.digitsCounter();
-// import "./files/gallery.js";
-// import "./libs/dynamic_adapt.js";
-// import './libs/wNumb.min.js';
+// import './files/gallery.js'
+// import './libs/dynamic-adapt.js'
+// import './files/script.js'
+import '../scss/style.scss'
 
-import './files/script.js'
+// flsFunctions.addTouchClass()
+// flsFunctions.addLoadedClass()
+// flsFunctions.menuInit()
+// flsFunctions.fullVHfix()
+// flsFunctions.scrollDirection()
+// flsFunctions.spoilers()
+// flsFunctions.tabs()
+// flsFunctions.showMore()
+
+// flsForms.formQuantity()
+// flsForms.formRating()
+
+// flsScroll.pageNavigation()
+// flsScroll.headerScroll()
+// flsScroll.stickyBlock()
+// flsScroll.digitsCounter()
--- js/files/scroll/lazyload.js
+++
@@ -1,9 +0,0 @@
-import LazyLoad from 'vanilla-lazyload'
-
-const lazyMedia = new LazyLoad({
-	elements_selector: '[data-src],[data-srcset]',
-	class_loaded: '_lazy-loaded',
-	use_native: true,
-})
-
-//lazyMedia.update();
