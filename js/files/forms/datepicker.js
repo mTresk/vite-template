b4eb154fc7d73b0d958bba7c1940887852ad64af
Maxim Tresk <djtresk@gmail.com>
2025-03-18 17:16:13 +0500

    chore: update datepicker

--- js/files/forms/datepicker.js
+++ js/files/forms/datepicker.js
@@ -2,17 +2,21 @@ import datepicker from 'js-datepicker'
 import { flsModules } from '../modules.js'
 import 'js-datepicker/src/datepicker'
 
-if (document.querySelector('[data-datepicker]')) {
-	const picker = datepicker('[data-datepicker]', {
-		customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-		customMonths: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
-		overlayButton: 'Применить',
-		overlayPlaceholder: 'Год (4 цифры)',
-		startDay: 1,
-		formatter: (input, date) => {
-			const value = date.toLocaleDateString()
-			input.value = value
-		},
+const datepickerSelectors = document.querySelectorAll('[data-datepicker]')
+
+if (datepickerSelectors.length) {
+	datepickerSelectors.forEach((datepickerSelector) => {
+		const picker = datepicker(datepickerSelector, {
+			customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+			customMonths: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+			overlayButton: 'Применить',
+			overlayPlaceholder: 'Год (4 цифры)',
+			startDay: 1,
+			formatter: (input, date) => {
+				const value = date.toLocaleDateString()
+				input.value = value
+			},
+		})
+		flsModules.datepicker = picker
 	})
-	flsModules.datepicker = picker
 }
