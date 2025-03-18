59002659799c3f9e701dc7b5ace698183ebd233e
Maxim Tresk <djtresk@gmail.com>
2025-03-18 17:14:07 +0500

    fix: inputmask selectors check

--- js/files/forms/inputmask.js
+++ js/files/forms/inputmask.js
@@ -2,7 +2,7 @@ import Inputmask from 'inputmask'
 
 const selectors = document.querySelectorAll('input[name="phone"]')
 
-if (selectors) {
+if (selectors.length) {
 	selectors.forEach((selector) => {
 		const im = new Inputmask('+7 (999) 999 99 99', { showMaskOnHover: false })
 		im.mask(selector)
